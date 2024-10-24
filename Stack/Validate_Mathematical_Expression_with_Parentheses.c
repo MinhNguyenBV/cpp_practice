c982112fa79a8d7c565153915d91f49ecdc1ac9d
Minh <81461582+MinhNguyenBV@users.noreply.github.com>
2024-10-24 13:44:44 +0700

    Create Validate_Mathematical_Expression_with_Parentheses

---
+++ Stack/Validate_Mathematical_Expression_with_Parentheses.c
@@ -0,0 +1,9 @@
+/*
+    Mô tả: Cho một biểu thức toán học chứa các dấu ngoặc tròn (), dấu ngoặc vuông [],
+    dấu ngoặc nhọn {}, và các phép toán cộng, trừ, nhân, chia. Bạn cần kiểm tra xem biểu thức 
+    đó có hợp lệ không (dấu ngoặc phải đóng mở đúng cách).
+
+    Ví dụ:
+    Input: "{(2 + 3) * [4 / (1 + 1)]}", Output: true
+    Input: "{2 + (3 * [4 - 5]}", Output: false
+*/
