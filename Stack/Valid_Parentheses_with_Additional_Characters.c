d780fc89d68dbc4890e05b8efc893baf5c564285
Minh <81461582+MinhNguyenBV@users.noreply.github.com>
2024-10-24 13:41:19 +0700

    Add files via upload

---
+++ Stack/Valid_Parentheses_with_Additional_Characters.c
@@ -0,0 +1,8 @@
+/*
+	Mô tả: Kiểm tra tính hợp lệ của chuỗi dấu ngoặc nhưng chuỗi có thể bao gồm cả các ký tự
+	không phải là dấu ngoặc, ví dụ như chữ cái hoặc chữ số. Bạn cần bỏ qua các ký tự không phải dấu ngoặc.
+
+	Ví dụ:
+	Input: "a(b)c[d]{e}", Output: true
+	Input: "a)b(c", Output: false
+*/
