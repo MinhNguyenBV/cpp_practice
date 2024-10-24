c0255d7038fb3e8f26f4fef5a741ab6010b47127
Minh <81461582+MinhNguyenBV@users.noreply.github.com>
2024-10-24 13:43:13 +0700

    Create Minimum_Add_to_Make_Parentheses_Valid

---
+++ Stack/Minimum_Add_to_Make_Parentheses_Valid.c
@@ -0,0 +1,8 @@
+/*
+    Mô tả: Cho một chuỗi chỉ chứa các dấu ngoặc tròn () không hợp lệ. 
+    Nhiệm vụ của bạn là thêm tối thiểu số dấu ngoặc để chuỗi trở thành hợp lệ.
+
+    Ví dụ:
+    Input: "())", Output: 1
+    Input: "(((", Output: 3
+*/
